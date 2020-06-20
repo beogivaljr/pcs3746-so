f42883e753e45ae02c6524358659be21e5a97e4c
Beogival Jr <beogivaljr@gmail.com>
2020-06-20 17:45:55 -0300

    Compiled

--- 1/linux/include/generated/compile.h
+++ 1/linux/include/generated/compile.h
@@ -1,6 +1,6 @@
-/* This file is auto generated, version 13 */
+/* This file is auto generated, version 14 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#13 Sat Jun 20 19:46:18 UTC 2020"
+#define UTS_VERSION "#14 Sat Jun 20 20:38:03 UTC 2020"
 #define LINUX_COMPILE_BY "student"
-#define LINUX_COMPILE_HOST "cf54da3b4f8c"
+#define LINUX_COMPILE_HOST "28897a9322e4"
 #define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu/Linaro 5.4.0-6ubuntu1~16.04.9) "
